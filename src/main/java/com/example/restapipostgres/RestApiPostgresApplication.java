ccdf1cad8c5ebca4154f1816e27ad532ac8d6491
Ori Bibas <oribibas6@gmail.com>
2022-07-11 20:25:54 -0400

    initial commit added maven project with spring dependencies

---
+++ src/main/java/com/example/restapipostgres/RestApiPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapipostgres/RestApiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapipostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
